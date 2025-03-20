7433e56f5701fbefb8f6a6d7b919b3db60f17188
jnewell-7 <jacksonnewell07@gmail.com>
2025-03-19 21:00:48 -0600

    initial

---
+++ backend/WaterProject.API/Controllers/WaterController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WaterProject.API.Data;
+
+namespace WaterProject.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class WaterController : ControllerBase
+    {
+        private WaterDbContext _waterContext;
+        
+        public WaterController(WaterDbContext temp)
+        {
+            _waterContext = temp;
+        }
+
+        [HttpGet("AllProjects")]
+        public IActionResult GetProjects(int pageSize = 10, int pageNum = 1)
+        {
+            var something =  _waterContext.Projects
+            .Skip((pageNum - 1) * pageSize)
+            .Take(pageSize)
+            .ToList();
+            
+            var totalNumProjects = _waterContext.Projects.Count();
+
+            var someObject = new
+            {
+                Projects = something,
+                TotalNumProjects = totalNumProjects
+            };
+            return Ok(someObject);
+        }
+
+        [HttpGet("FunctionalProjects")]
+        public IEnumerable<Project> GetFunctionalProjects()
+        {
+            var something = _waterContext.Projects.Where(p => p.ProjectFunctionalityStatus == "Functional").ToList();
+            return something;
+        }
+    }
+}
